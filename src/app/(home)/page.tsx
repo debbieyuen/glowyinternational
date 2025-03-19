4cd25c1c127b72d2824a637bdc1974d6ddb6ffe3
Debbie Yuen <deborahyuen2016@berkeley.edu>
2025-03-19 09:16:10 -0700

    debug mapbox

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,36 +1,35 @@
 // import Link from 'next/link';
-"use client";
+// use client so we can run it on the server
+"use client"
+import * as React from 'react';
+import Map from 'react-map-gl/mapbox';
 import "mapbox-gl/dist/mapbox-gl.css";
-import Map from "react-map-gl";
 import { useTheme } from "next-themes";
+
 export default function HomePage() {
-  // Read the Mapbox Theme
-  const { theme } = useTheme();
-  // Read Mapbox Token
+  // Mapbox Token
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+  // Next Themes
+  const { theme } = useTheme();
+
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      {/* <Map>
-        mapboxAccessToken={mapboxToken}
-        initialViewState={{
-          longitude:-122.2,
-          latitude:38.1,
-          zoom: 10,
-        }}
-        mapStyle={
-          theme === "light" 
-            ? "mapbox://styles/mapbox/streets-v11" 
-            : "mapbox://styles/mapbox/dark-v10"
-        } */}
-      {/* >
-        {markers.map((marker, index) => (
-          <Marker>
-            longitude={marker.longitude}
-            latitude={marker.latitude}
-            color="red"
-          </Marker>
-        ))} */}
-      {/* </Map> */}
+      <Map
+      mapboxAccessToken={mapboxToken}
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle={
+        theme === "light"
+        ? "mapbox://styles/mapbox/streets-v9"
+        : "mapbox://styles/mapbox/dark-v10"
+      }
+    />
+
       <h1 className="mb-4 text-9xl font-bold">Glowy</h1>
       <h2 className="mb-9 text-3xl font-bold">Learning and inventing to bring light around the world</h2>
       <button>click me</button>
