1cf3a4a4f6cb1432854f16a8d27533f8ddaf433e
Debbie Yuen <deborahyuen2016@berkeley.edu>
2025-03-19 21:07:38 -0700

    reorganize documentation files and icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,3 +16,16 @@ export default function Layout({ children }: { children: ReactNode }) {
     </html>
   );
 }
+
+// import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
+// import { baseOptions } from '@/app/layout.config';
+// import { source } from '@/lib/source';
+// import type { ReactNode } from 'react';
+ 
+// export default function Layout({ children }: { children: ReactNode }) {
+//   return (
+//     <DocsLayout {...baseOptions} tree={source.pageTree}>
+//       {children}
+//     </DocsLayout>
+//   );
+// }
